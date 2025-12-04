21a3f0d519966d4a89ec95a05daefc6c4cb6f27f
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-12-04 14:40:04 -0500

    Un-hardcoded diamondfire-unknown-unknown.json cg and linker paths;

--- diamondfire/build.sh
+++ diamondfire/build.sh
@@ -1,8 +1,12 @@
+cat ../diamondfire-unknown-unknown.json.template \
+    | sed -e "s|{DIAMONDFIRERS_ROOT}|$(dirname ${PWD})|g" \
+    > ../diamondfire-unknown-unknown.json
+
 rm rustc-ice-*.txt
 rm ../diamondfire-macros/rustc-ice-*.txt
 printf "\n\x1b[32mCompiling Codegen Library...\x1b[0m\n"
 (cd ../rustc_codegen_diamondfire; cargo build || exit 1)
 printf "\n\x1b[32mCompiling Linker Executable...\x1b[0m\n"
-(cd ../diamondfire-linker; cargo build || exit 1)
+(cd ../linker_diamondfire; cargo build || exit 1)
 printf "\n\x1b[32mCompiling Example Library...\x1b[0m\n"
 cargo build -Zbuild-std --target=../diamondfire-unknown-unknown.json --example=transform || exit 1
