60c79d06bf6731c09389e53b372357b4af2386b0
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-12-21 00:27:18 -0500

    Added incomplete len method and Drop impl to Vec;

--- diamondfire/src/std/vec.rs
+++ diamondfire/src/std/vec.rs
@@ -1,18 +1,16 @@
 use core::marker::PhantomData;
 
 use diamondfire_sys::{
-    df_opaque,
-    df_list,
     action::{
-        DF_ACTION__SetVar__CreateList,
-        DF_ACTION__SetVar__AppendValue
-    }
+        DF_ACTION__SetVar__AppendValue, DF_ACTION__SetVar__CreateList, DF_ACTION__SetVar__ListLength
+    }, df_list, df_opaque
 };
 
 pub struct Vec<T> {
     _opaque  : *mut df_list,
     _phantom : PhantomData<T>,
 }
+
 
 impl<T> Vec<T> {
 
@@ -32,13 +30,29 @@ impl<T> Vec<T> {
 
     pub fn push(&mut self, value: T) { unsafe {
         DF_ACTION__SetVar__AppendValue(
-            self.inner,
+            self._opaque,
             (&raw const value) as (*const df_opaque)
         )
+    } }
+
+    pub fn len(&self) -> usize { unsafe {
+        let mut out = MaybeUninit::<df_number>::uninit();
+        DF_ACTION__SetVar__ListLength(
+            out.as_mut_ptr(),
+            self._opaque as (*const df_list)
+        );
+        todo!("convert df_number to usize")
     } }
 
     pub fn get(&self, index: usize) -> Option<&T> {
         todo!("cannot be implemented until pointer representation is decided")
     }
 
+}
+
+
+impl<T> Drop for Vec<T> {
+    fn drop(&mut self) {
+        todo!("properly drop inner values")
+    }
 }
